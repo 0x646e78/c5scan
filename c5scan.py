d45661357f82483e89ce81b4a41595ace4d9195a
auraltension <auraltension@riseup.net>
2014-06-03 23:17:58 +1000

    Made a class, some other guff, send better commit messages...

--- c5scan.py
+++ c5scan.py
@@ -2,7 +2,9 @@
 
 from contextlib import closing
 from lxml import html
+import httplib2
 import requests # requires > = 1.2
+import gevent
 import argparse
 import re
 import sys
@@ -13,6 +15,55 @@ versions = [
     '5.5.0', '5.5.1', '5.5.2', '5.5.2.1', '5.6.0', '5.6.0.1', '5.6.0.2', 
     '5.6.1', '5.6.1.1', '5.6.1.2', '5.6.2', '5.6.2.1', '5.6.3', '5.6.3.1'
 ]
+
+known_vulns = {
+    '5.4.2.1': 'http://www.exploit-db.com/exploits/17925/',
+    '5.6.1.2': 'http://www.exploit-db.com/exploits/26077/',
+    '5.6.2.1': 'http://www.exploit-db.com/exploits/31735/'
+}
+
+readme_locations = [
+    'concrete/libraries/3rdparty/adodb/readme.txt',
+    'concrete/libraries/3rdparty/adodb/docs/docs-adodb.htm',
+    'concrete/blocks/video/README',
+    'concrete/libraries/3rdparty/StandardAnalyzer/Readme.txt',
+    'concrete/libraries/3rdparty/securimage/README.txt'
+]
+
+class Conn:
+    """Connect to the target"""
+    def __init__(self, url):
+        self.url = url
+        self.verify = False
+
+        try:
+            # Server is responsive
+            h = httplib2.Http(disable_ssl_certificate_validation=True)
+            self.headers = h.request(self.url, 'HEAD')
+            assert int(self.headers[0]['status']) < 400
+            # Concrete5 appears to be installed
+            r = requests.get(self.url + 'concrete/', verify=self.verify)
+            if r.status_code == 404:
+                redtext('The site is up but does not appear to be running concrete5')
+                exit(1)
+        except Exception as e:
+            redtext("%s is not reachable" % self.url)
+            exit(1)
+
+    def get(self, path):
+        try:
+            r = requests.get(self.url + path, verify=self.verify)
+            return r
+        except Exception as e:
+            redtext("Danger Will Robinson! %s" %  e)
+            exit(1)
+
+    def heads(self):
+        return self.headers
+
+    def url(self):
+        return self.url
+
 
 def banner():
     banner=(
@@ -30,6 +81,9 @@ def redtext(text):
 def orangetext(text):
     print '\033[33m' + text + '\033[0m'
 
+def yellowtext(text):
+    print '\033[36m' + text + '\033[0m'
+
 def format_url(url):
     if not re.search('^http', url):
         print "No http:// or https:// provided. Trying http://"
@@ -38,19 +92,8 @@ def format_url(url):
         url += '/'
     return url
 
-def site_available(url):
-    try:
-        closing(requests.get(url, stream=True, verify=False))
-        r = requests.get(url + 'concrete/', verify=False)
-        if r.status_code == 404:
-            redtext('The site is up but does not appear to be running concrete5')
-            exit(1)
-    except Exception as e:
-        redtext("%s is not reachable" % url)
-        exit(1)
-
-def returns_404(url):
-    r = requests.get(url + '404check', stream=True, verify=False)
+def returns_404(c):
+    r = c.get('404check')
     if r.status_code == 404:
         r.close
         return True
@@ -58,19 +101,20 @@ def returns_404(url):
         r.close
         return False
 
-def check_headers(url):
-    r = requests.get(url, verify=False)
+def check_headers(c):
+    h = c.heads()
     for i in ['server', 'x-powered-by']:
         try:
-            print '[+] Interesting header: %s: %s' % (i, r.headers[i])
+            orangetext('[+] Interesting header: %s: %s' % (i, h[0][i]))
         except KeyError:
             pass
 
 
-def get_robots(url, return_codes):
-    r = requests.get(url + '/robots.txt', verify=False)
+def get_robots(c, return_codes):
+    r = c.get('robots.txt')
     if (r.status_code == 200) and return_codes:
-        print "[+] robots.txt found:\n ", r.content
+        print "[+] robots.txt found at ", c.url + 'robots.txt'
+        yellowtext(r.content)
 
 def get_version(url):
     try:
@@ -81,13 +125,33 @@ def get_version(url):
     except:
         return False
 
-def check_updates(url, versions, return_codes):
-    print "Enumerating updates"
+def check_updates(conn, versions, return_codes):
+    print "\nEnumerating updates in /updates/"
+    updates = []
     for v in versions:
         for extension in ['', '_updater']:
-            r = requests.get(url + '/updates/concrete' + v + extension, verify=False)
+            path = '/updates/concrete' + v + extension
+            r = conn.get(path)
             if r.status_code == 200 and (return_codes or (r.content == '')):
                 print "[+] Update version %s exists" % v
+                updates.append(v)
+    return updates
+
+def check_readmes(c, readme_locations):
+    print "\nLooking for Readme files"
+    for i in readme_locations:
+        r = c.get(i)
+        if r.status_code == 200:
+            print "[+] Found a readme at: ", c.url + i
+
+def check_vulns(updates, known_vulns):
+    print "\nChecking for known vulnerabilities"
+    for i in updates:
+        if i in known_vulns:
+            orangetext(
+                '[+] A known vulnerability exists for %s:\n %s' % 
+                (i, known_vulns[i])
+            )
 
 def main():
     parser = argparse.ArgumentParser(description='A c5 scanner')
@@ -103,25 +167,44 @@ def main():
     # Format the url and ensure it is reachable
     url = format_url(args.url)
     print 'URL: ' + url + '\n'
-    site_available(url)
+
+    # Connect to host
+    conn = Conn(url)
 
     # Some versions didn't return status codes correctly
-    return_codes = returns_404(url)
+    return_codes = returns_404(conn)
     if not return_codes:
         print "[+] Site is not correctly handling HTTP return codes\n"
 
     # Get version from meta tags
     version = get_version(url)
     if version and re.search('\d', version):
+        version = version.split(' ')[-1]
         print "[+] Discovered version %s from meta 'generator' tag" % version
 
-    check_headers(url)
+    check_headers(conn)
 
     # Check for robots.txt
-    get_robots(url, return_codes)
+    get_robots(conn, return_codes)
 
     # Enumerate update versions
-    check_updates(url, versions, return_codes)
+    updates = check_updates(conn, versions, return_codes)
+
+    # Check for known readme locations
+    check_readmes(conn, readme_locations)
+
+    # Check for known vulns
+    if version:
+        updates.append(version)
+    updates = list(set(updates))
+    check_vulns(updates, known_vulns)
+
+    if version:
+        if version in known_vulns:
+            redtext('\n[!] Current version is vulnerable')
+        else:
+            print 'A public disclosure does not exist for the current version'
+
 
 if __name__ == "__main__":
     main()
